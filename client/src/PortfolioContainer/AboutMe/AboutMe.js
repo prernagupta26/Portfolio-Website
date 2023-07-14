0b474c61092efb7cbae5c0130c53b1f1856737f2
Prerna Gupta <guptaprerna2607@gmail.com>
2023-07-14 09:59:51 +0530

    Few changes in about me section

--- client/src/PortfolioContainer/AboutMe/AboutMe.js
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -1,82 +1,3 @@
-// import React, { useEffect } from "react";
-// import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
-// import ScrollService from "../../utilities/ScrollService";
-// import Animations from "../../utilities/Animations";
-// import "./AboutMe.css";
-
-// export default function AboutMe(props) {
-  
-//   let fadeInScreenHandler = (screen) => {
-//     if (screen.fadeInScreen !== props.id) return;
-//     Animations.animations.fadeInScreen(props.id);
-//   };
-
-//   const fadeInSubscription =
-//     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
-
-//   const SCREEN_CONSTSANTS = {
-//     description:
-//       "Highly motivated and technically skilled individual with a strong foundation in Java, C++, object-oriented programming, and data structures. Passionate about leveraging my technical expertise to contribute effectively to the success of your organization. Eager to apply my knowledge, learn new technologies, and make a positive impact in the field of technology.",
-//     highlights: {
-//       bullets: [
-//         "Java",
-//         "SpringBoot, Microservices",
-//         "c++",
-//         "Data Structures and Algorithms",
-//         "Object Oriented Programming",
-//         "Operating Systems",
-        
-//       ],
-//       heading: "Skills:",
-//     },
-//   };
-//   const renderHighlight = () => {
-//     return SCREEN_CONSTSANTS.highlights.bullets.map((value, i) => (
-//       <div className="highlight" key={i}>
-//         <div className="highlight-blob"></div>
-//         <span>{value}</span>
-//       </div>
-//     ));
-//   };
-
-//   return (
-//     <div
-//       className="about-me-container screen-container fade-in"
-//       id={props.id || ""}
-//     >
-//       <div className="about-me-parent">
-//         <ScreenHeading title={"About Me"} subHeading={"Why Choose Me?"} />
-//         <div className="about-me-card">
-//           <div className="about-me-profile"></div>
-//           <div className="about-me-details">
-//             <span className="about-me-description">
-//               {SCREEN_CONSTSANTS.description}
-//             </span>
-//             <div className="about-me-highlights">
-//               <div className="highlight-heading">
-//                 <span>{SCREEN_CONSTSANTS.highlights.heading}</span>
-//               </div>
-//               {renderHighlight()}
-//             </div>
-//             <div className="about-me-options">
-//               <button
-//                 className="btn primary-btn" onClick={() => ScrollService.scrollHandler.scrollToHireMe()} >
-//                 {" "}
-//                 Hire Me{" "}
-//               </button>
-//               <a href="resume.pdf" download="Prerna Gupta.pdf">
-//                 <button className="btn highlighted-btn">Get Resume</button>
-//               </a>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-
-
 import React, { useEffect } from "react";
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import ScrollService from "../../utilities/ScrollService";
@@ -84,26 +5,29 @@ import Animations from "../../utilities/Animations";
 import "./AboutMe.css";
 
 export default function AboutMe(props) {
+  
   let fadeInScreenHandler = (screen) => {
     if (screen.fadeInScreen !== props.id) return;
     Animations.animations.fadeInScreen(props.id);
   };
+
   const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
   const SCREEN_CONSTSANTS = {
     description:
-      "Full stack web and mobile developer with background knowledge of MERN stacks with redux, along with a knack of building applications with utmost efficiency. Strong professional with a BSC willing to be an asset for an organization.",
+      "Highly motivated and technically skilled individual with a strong foundation in Java, C++, object-oriented programming, and data structures. Passionate about leveraging my technical expertise to contribute effectively to the success of your organization. Eager to apply my knowledge, learn new technologies, and make a positive impact in the field of technology.",
     highlights: {
       bullets: [
-        "Full Stack web and mobile development",
-        "Interactive Front End as per the design",
-        "React and React Native",
-        "Redux for State Mnanagement",
-        "Building REST API",
-        "Managing database",
+        "Java",
+        "SpringBoot, Microservices",
+        "c++",
+        "Data Structures and Algorithms",
+        "Object Oriented Programming",
+        "Operating Systems",
+        
       ],
-      heading: "Here are a Few Highlights:",
+      heading: "Skills:",
     },
   };
   const renderHighlight = () => {
@@ -136,13 +60,11 @@ export default function AboutMe(props) {
             </div>
             <div className="about-me-options">
               <button
-                className="btn primary-btn"
-                onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
-              >
+                className="btn primary-btn" onClick={() => ScrollService.scrollHandler.scrollToHireMe()} >
                 {" "}
                 Hire Me{" "}
               </button>
-              <a href="ehizcv.pdf" download="Ehiedu Ehizcv.pdf">
+              <a href="resume.pdf" download="Prerna Gupta.pdf">
                 <button className="btn highlighted-btn">Get Resume</button>
               </a>
             </div>
@@ -152,3 +74,5 @@ export default function AboutMe(props) {
     </div>
   );
 }
+
+
